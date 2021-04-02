8a5f21a8b2bdcb3d7e8c3dbb2454452a96993676
c17an <kimchanmin1@gmail.com>
2021-04-02 23:38:42 +0900

    한국어 번역 추가

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# react-query 예제
+
+이 소스는 next 기반의 예제로, [Nathan Sebhastian](https://github.com/nsebhastian/react-query-example)님의 주석을 한국어로 번역한 저장소입니다.
+
+## 실행 방법
+
+### 저장소 클론하기
+
+```
+git clone https://github.com/C17AN/react-query-example.git
+```
+
+### 클론한 디렉토리에서 커맨드 실행
+
+```
+cd react-query-example
+npm i
+cd pages
+npm run build
+npm start
+```
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,66 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-import { useQuery, useMutation, queryCache } from 'react-query'
-import { ReactQueryDevtools } from 'react-query-devtools'
+import { useQuery, useMutation, queryCache } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 export default () => {
-  const [text, setText] = React.useState('')
-  const { status, data, error, isFetching } = useQuery('todos', async () => {
-    const { data } = await axios.get('/api/data')
-    return data
-  })
+  const [text, setText] = React.useState("");
+  const { status, data, error, isFetching } = useQuery("todos", async () => {
+    const { data } = await axios.get("/api/data");
+    return data;
+  });
 
   const [mutatePostTodo] = useMutation(
-    text => axios.post('/api/data', { text }),
+    (text) => axios.post("/api/data", { text }),
     {
       onSuccess: () => {
-        // Query Invalidations
+        // 쿼리 무효화(Query Invalidations)
         // queryCache.invalidateQueries('todos')
-        setText('')
+        setText("");
       },
     }
-  )
+  );
 
   return (
     <div>
       <p>
-        When adding new items, the useMutation hook will observe the result of the asynchronous function.
-        You can invalidate the query on success, and React Query will refetch the query with the same key.
+        새로운 항목을 추가할 때, useMutation 훅이 비동기 함수의 실행 결과를
+        관측할 것입니다.
+        <br />
+        요청에 성공한 후 이전 쿼리를 무효화(invalidate)할 수도 있으며, React
+        Query는 동일한 키로 쿼리를 다시 불러옵니다.
       </p>
       <form
-        onSubmit={e => {
-          e.preventDefault()
-          mutatePostTodo(text)
+        onSubmit={(e) => {
+          e.preventDefault();
+          mutatePostTodo(text);
         }}
       >
         <input
           type="text"
-          onChange={event => setText(event.target.value)}
+          onChange={(event) => setText(event.target.value)}
           value={text}
         />
         <button>Create</button>
       </form>
       <br />
-      {status === 'loading' ? (
-        'Loading...'
-      ) : status === 'error' ? (
+      {status === "loading" ? (
+        "Loading..."
+      ) : status === "error" ? (
         error.message
       ) : (
         <>
           <div>Updated At: {new Date(data.ts).toLocaleTimeString()}</div>
           <ul>
-            {data.items.map(datum => (
+            {data.items.map((datum) => (
               <li key={datum}>{datum}</li>
             ))}
           </ul>
-          <div>{isFetching ? 'Updating in background...' : ' '}</div>
+          <div>{isFetching ? "백그라운드에서 업데이트 중..." : " "}</div>
         </>
       )}
       <ReactQueryDevtools initialIsOpen />
     </div>
-  )
-}
+  );
+};
